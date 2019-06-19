a9c53483aa8632d82b686356065ce302d2490c13
Alex <46602330+AlexEra@users.noreply.github.com>
2019-06-19 16:06:03 +0300

    Update undistort.py

--- undistort.py
+++ undistort.py
@@ -1,12 +1,12 @@
 import cv2
 
-
 def undist_img(img, cameraMatrix, distCoeffs):
     h, w = img.shape[:2]
-    newcameramtx, roi=cv2.getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, (w, h),1 ,(w, h))
-    # undistort
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, 
+                                                      (w, h), 1,(w, h))
+    # Undistort.
     dst = cv2.undistort(img, cameraMatrix, distCoeffs, None, newcameramtx)
-    # crop the image
+    # Crop the image.
     x, y, w, h = roi
     dst = dst[y:y+h, x:x+w]
     return dst
